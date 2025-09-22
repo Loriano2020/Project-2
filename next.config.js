ed5db10168ce1b0eed5757fa115d6d202234d274
Loriano2020 <lorianolopes223@gmail.com>
2025-09-22 11:40:49 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+  reactStrictMode: true,
+  // Add other configurations as needed
+};
